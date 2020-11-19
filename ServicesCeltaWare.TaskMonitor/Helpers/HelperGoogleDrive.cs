ad9d64cf396f1301ec6acd9ef45e1bca4cbf4aaf
albertosfernandes <alberto.souzafernandes@gmail.com>
2020-11-19 00:05:41 -0300

    Add Api GoogleDriveServiceController

---
+++ ServicesCeltaWare.TaskMonitor/Helpers/HelperGoogleDrive.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServicesCeltaWare.TaskMonitor.Helpers
+{
+    public class HelperGoogleDrive
+    {
+        private static readonly HttpClient client = new HttpClient();
+        public static async Task<bool> Upload(Model.ModelBackupSchedule _backupSchedule)
+        {
+            string url = "http://" + _backupSchedule.CustomerProduct.Server.IpAddress + ":" + _backupSchedule.CustomerProduct.Server.Port;
+            var json = JsonConvert.SerializeObject(_backupSchedule);
+            var stringContent = new StringContent(json, UnicodeEncoding.UTF8, "application/json");
+
+            var streamResult = await client.PostAsync(url + "/api/GoogleDriveService/Upload", stringContent);
+
+            if (!streamResult.IsSuccessStatusCode)
+            {
+                //resp = await UpdateStatusBackup(_backupSchedule, Model.Enum.BackupStatus.Success, true);
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ ServicesCeltaware.ServerAPI/Controllers/GoogleDriveServiceController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ServicesCeltaWare.Model;
+using ServicesCeltaWare.UtilitariosInfra;
+
+namespace ServicesCeltaware.ServerAPI.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    [EnableCors("BasePolicy")]
+    public class GoogleDriveServiceController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> Upload(ModelBackupSchedule _backupSchedule)
+        {
+            try
+            {
+                string filename = _backupSchedule.Databases.Directory + "\\backup\\";
+                if(_backupSchedule.Type == ServicesCeltaWare.Model.Enum.BackuypType.Full)
+                {
+                    //full
+                    filename += _backupSchedule.Databases.DatabaseName + "BackupFull.bak";
+                }
+                else
+                {
+                    filename += $"{_backupSchedule.Databases.DatabaseName}BackupDiff{ _backupSchedule.DateHourExecution.Hour.ToString() + _backupSchedule.DateHourExecution.Minute.ToString()}.bak";
+                    //diff
+                }
+                  
+                await CallManager.Upload(filename);
+                return Ok();
+            }
+            catch(Exception err)
+            {
+                return BadRequest(err.Message);
+            }
+        }
+
+    }
+}
